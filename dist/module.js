aa52fa471afb0c7e43caab92ca527ea75e587fe4
Nam Le <nam.leduc02@gmail.com>
2021-03-03 17:16:03 +0100

    update last non zero

--- dist/module.js
+++ dist/module.js
@@ -1336,7 +1336,18 @@ var processData = function processData(buffer) {
   // }
   // return buffer.slice(-6).reduce((total, elm) => total + elm, 0);
   // return buffer.slice(-7).reduce((total, elm) => total + elm, 0);
-  return buffer.slice(-1)[0] || 0;
+  // return buffer.slice(-1)[0] || 0;
+  var tmp = buffer.slice(0).reverse();
+
+  for (var i = 0; i < tmp.length; i++) {
+    if (tmp[i] > 0) {
+      return tmp[i];
+    }
+
+    break;
+  }
+
+  return 0;
 };
 
 /***/ }),
--- src/util/helpFuc.ts
+++ src/util/helpFuc.ts
@@ -4,5 +4,13 @@ export const processData = (buffer: number[]) => {
   // }
   // return buffer.slice(-6).reduce((total, elm) => total + elm, 0);
   // return buffer.slice(-7).reduce((total, elm) => total + elm, 0);
-  return buffer.slice(-1)[0] || 0;
+  // return buffer.slice(-1)[0] || 0;
+  const tmp = buffer.slice(0).reverse();
+  for (let i = 0; i < tmp.length; i++) {
+    if (tmp[i] > 0) {
+      return tmp[i];
+    }
+    break;
+  }
+  return 0;
 };
