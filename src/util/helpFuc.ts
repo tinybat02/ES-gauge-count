520b4bd190e50c56c9f8595d65e34da37b19b2d6
Nam Le <nam.leduc02@gmail.com>
2021-03-11 18:26:05 +0100

    add max of 2 latest intervals

--- src/util/helpFuc.ts
+++ src/util/helpFuc.ts
@@ -5,11 +5,6 @@ export const processData = (buffer: number[]) => {
   // return buffer.slice(-6).reduce((total, elm) => total + elm, 0);
   // return buffer.slice(-7).reduce((total, elm) => total + elm, 0);
   // return buffer.slice(-1)[0] || 0;
-  const tmp = buffer.slice(0).reverse();
-  for (let i = 0; i < tmp.length; i++) {
-    if (tmp[i] > 0) {
-      return tmp[i];
-    }
-  }
-  return 0;
+  if (buffer.length == 0) return 0;
+  return Math.max(...buffer.slice(-2));
 };
